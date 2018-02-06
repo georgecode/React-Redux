b60b12cdc1fefc153dd68de13efdcafe715f509b
georgecode <georgeczurlanis@gmail.com>
2018-02-05 23:03:21 -0800

    Added notes and a store

---
+++ src/js/store/index.js
@@ -0,0 +1,13 @@
+// src/js/store/index.js
+
+
+// createStore is the function for creating the Redux store.
+
+// createStore takes a reducer as the first argument, rootReducer in our case.
+
+import { createStore } from "redux";
+import rootReducer from "../reducers/index";
+
+const store = createStore(rootReducer);
+
+export default store;
