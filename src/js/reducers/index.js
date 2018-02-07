fbde14278362938b043b8bc519ad8e8d190bd67d
georgecode <georgeczurlanis@gmail.com>
2018-02-06 19:43:01 -0800

    Add reducer, add package to allow spread operator add notes update config babelrc

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,30 @@
 // src/js/reducers/index.js
 
+// const initialState = {
+//   articles: []
+// };
+
+// const rootReducer = (state = initialState, action) => state;
+
+// export default rootReducer;
+
+
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
-const rootReducer = (state = initialState, action) => state;
+
+//reducers are always written as a function that takes two params (state, action)
+//They are almost always a switch statement that returns the initial state as a default
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return { ...state, articles: [...state.articles, action.payload] };
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
