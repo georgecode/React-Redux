bf2428a3a3989f884c109b7cf869bbc4021da8d6
georgecode <georgeczurlanis@gmail.com>
2018-02-06 19:59:36 -0800

    Add react-redux and more notes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-console.log("I am a boilerplate :-)");
+//console.log("I am a boilerplate :-)");
+import store from "../js/store/index";
+import { addArticle } from "../js/actions/index";
+
+window.store = store;
+window.addArticle = addArticle;
